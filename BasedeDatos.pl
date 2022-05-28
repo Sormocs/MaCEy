f38a0320efe2c1040b34ee5189444eda46e8980a
David Richmond Soto <luis.richmond9@gmail.com>
2022-05-27 19:50:16 -0600

    hotfix
    hotfix

--- BasedeDatos.pl
+++ BasedeDatos.pl
@@ -21,28 +21,27 @@ pista_2_2([]).
 pista_3([]).
 
 
-
-asignar_pista_1(Aeronave,Hora):-
+asignar_pista_1(Aeronave,Hora,Tamano):-
     aeronave(Aeronave,Tamano),
     pista_1(X),
-    not miembro(Hora,X), concatenar(X,[Hora],Y), pista_1(Y), retract(pista_1(X)), assert(pista_1(Y)).
+    not(miembro(Hora,X)),concatenar(X,[Hora],Y),pista_1(Y),retract(pista_1(X)),assert(pista_1(Y)).
 
 
-asignar_pista_2_1(Aeronave,Hora):-
+asignar_pista_2_1(Aeronave,Hora,Tamano):-
     aeronave(Aeronave,Tamano),
     pista_2_1(X),
-    not miembro(Hora,X), concatenar(X,[Hora],Y), pista_2_1(Y), retract(pista_2_1(X)), assert(pista_2_1(Y)).
+    not(miembro(Hora,X)),concatenar(X,[Hora],Y),pista_2_1(Y),retract(pista_2_1(X)),assert(pista_2_1(Y)).
 
-asignar_pista_2_2(Aeronave,Hora):-
+asignar_pista_2_2(Aeronave,Hora,Tamano):-
     aeronave(Aeronave,Tamano),
     pista_2_2(X),
-    not miembro(Hora,X), concatenar(X,[Hora],Y), pista_2_2(Y), retract(pista_2_2(X)), assert(pista_2_2(Y)).
+    not(miembro(Hora,X)),concatenar(X,[Hora],Y),pista_2_2(Y),retract(pista_2_2(X)),assert(pista_2_2(Y)).
 
-asignar_pista_3(Aeronave,Hora):-
+asignar_pista_3(Aeronave,Hora,Tamano):-
     aeronave(Aeronave,Tamano),
     pista_3(X),
-    not miembro(Hora,X), concatenar(X,[Hora],Y), pista_3(Y), retract(pista_3(X)), assert(pista_3(Y)).
-    
+    not(miembro(Hora,X)),concatenar(X,[Hora],Y),pista_3(Y),retract(pista_3(X)),assert(pista_3(Y)).
+
 
 %Definicion Emergencias
 emergencia("Perdida de Motor").
@@ -56,6 +55,28 @@ atencion_emergencia("Llamar bomberos").
 atencion_emergencia("Llamar a la policia y OIJ").
 atencion_emergencia("Llamar a la central").
 atencion_emergencia("Llamar a medico").
+
+%aerolineas
+aerolinea("tec-airlines").
+aerolinea("ace").
+aerolinea("avianca").
+
+%matricula
+matricula("alfa_charlie_tango_lima").
+matricula("delta_echo_hotel_victor").
+matricula("fox_bravo_hotel_romeo").
+
+%verificar si la arolinea existe
+existe_aerolinea(Aerolinea):-
+    aerolinea(Aerolinea).
+
+%verificar si la matricula existe
+existe_matricula(Matricula):-
+    matricula(Matricula).
+
+%verificar si la aeronave existe
+existe_aeronave(Aeronave):-
+    aeronave(Aeronave,_).
 
 concatena([],L,L).
 concatena([H|T],L2,[H|L3]) :- concatena(T,L2,L3).
--- chat.pl
+++ chat.pl
@@ -1,39 +1,74 @@
 :- [gramatica].
 :- [basedeDatos].
 
-
 %Función miembro que recibe una lista y un elemento y devuelve true si el elemento está en la lista
 miembro(X, [X|_]).
 miembro(X, [_|Y]):- miembro(X, Y).
+
+%verifica si la hora es posible
+horaesposible(X) :- horaposible(Y), miembro(X, Y).
 
 %Fucion concatena que recibe dos listas y devuelve una lista con todos los elementos de las dos listas
 concatena([], L, L).
 concatena([X|L1], L2, [X|L3]):- concatena(L1, L2, L3).
 
+%retorna la cabecera de una lista
+cabecera([X|_], X).
+
 %verifica si tiene sentido la oracion y si no la pide de nuevo
-verificarSentidoLogico(L) :- writeln(L),oracion(L, []).
-verificarSentidoLogico(L) :- writeln("No entendi la peticion, intenta de nuevo."), read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",X),verificarSentidoLogico(X).
+verificarSentidoLogico(L) :- writeln(L),oracion(L, []), verificarAccion(L),!.
+verificarSentidoLogico(L) :- writeln("No entendi la peticion, intenta de nuevo."), readln(L,_,_,_,lowercase),verificarSentidoLogico(L).
 
 %verifica que accion desea hacer el usaurio 
-verificarAccion(L):- miembro("hola", L), miembro("mayce",L), conseguirIdentificacion().
-verificarAccion(L):- miembro("permiso", L), miembro("despegar",L), write("Adios"), nl, !.
-verificarAccion(L):- miembro("mayday", L), write("Adios"), nl, !.
+verificarAccion(L):- miembro("hola", L), miembro("maycey",L), ayudar().
+verificarAccion(L):- miembro("permiso", L), miembro("despegar",L), conseguirIdentificacion(["despegar"]).
+verificarAccion(L):- miembro("permiso", L), miembro("aterrizar",L), conseguirIdentificacion(["aterrizar"]).
+verificarAccion(L):- miembro("mayday", L), !.
+
+%despues de saludar
+ayudar():-writeln("En que lo puedo ayudar?"), read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), ayudaAccion(X).
+ayudaAccion(L) :- miembro("solicito", L), miembro("despegar",L), conseguirIdentificacion(["despegar"]).
+ayudaAccion(L) :- miembro("solicito", L), miembro("aterrizar",L), conseguirIdentificacion(["aterrizar"]).
+ayudaAccion(L) :- miembro("mayday", L).
 
 %consigue la identificacion del usuario
-conseguirIdentificacion() :- write("Porfavor identifiquese : "), read(X), string_lower(X,X1), split_string(X1," ",".,",L), conseguirAvion().
+conseguirIdentificacion(Lv) :- writeln("Porfavor identifiquese con su matricula: Consulte la base de datos"), read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), verificarId(L,Lv).
 
-conseguirAvion():-write("¿Qué tipo de Aeronave es?"), read(X), string_lower(X,X1), split_string(X1," ",".,",L), conseguirHoraDeSalida().
+%verifica que la identificacion sea correcta
+verificarId(L,Lv):- cabecera(L,X),writeln(X), existe_matricula(X),concatena(Lv,[X],Ln), conseguirAvion(Ln).
+verificarId(L,Lv):- writeln("No existe la matricula, intenta de nuevo : "), read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), verificarId(L,Lv).
 
-conseguirHoraDeSalida():-write("Indique la hora de salida"), read(X), string_lower(X,X1), split_string(X1," ",".,",L).
+%consigue el avion del usuario
+conseguirAvion(Lv):-writeln(Lv),writeln("¿Qué tipo de Aeronave es?"),read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), verificarAeronave(L,Lv).
 
-conseguirDireccion():-write("Indique la direccion"), read(X), string_lower(X,X1), split_string(X1," ",".,",L).
+%verifica que el avion sea correcto
+verificarAeronave(L,Lv):- cabecera(L,X),writeln(X), existe_aeronave(X),concatena(Lv,[X],Ln), conseguirHoraDeSalida(Ln).
+verificarAeronave(L,Lv):- writeln("No existe la aeronave, intenta de nuevo : "), read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), verificarAeronave(L,Lv).
 
-despedirse():-write("Adios"), nl, !.
+%consigue la hora de salida del avion
+conseguirHoraDeSalida(Lv):-writeln("Indique la hora de salida"), read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), verificarHora(L,Lv).
 
-main :- read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), verificarAccion(L1), main.
+%verifica que la hora sea correcta
+verificarHora(L,Lv):- cabecera(L,X),writeln(X), horaesposible(X),concatena(Lv,[X],Ln), conseguirDireccion(Ln).
+verificarHora(L,Lv):- writeln("Esta hora no dsaes posible, porfavor ingrese una hora posible : "), read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), verificarHora(L,Lv).
 
+%consigue la hora de direccion del avion
+conseguirDireccion(Lv):-writeln("Indique la direccion"),read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), conseguirRespuesta(Lv).
 
-test:- write("Porfavor identifiquese : "), read(X), string_lower(X,X1), split_string(X1," ",".,",L),
+%consigue la respuesta para el usuario
+conseguirRespuesta(Ln):- writeln("Procesando..."),nuevaLlamada().%llamarABase(Ln,R), writeln(R).
+
+nuevaLlamada():-writeln("¿Desea realizar otra llamada? (si o no)"), read(Y),string_lower(Y,Y1),split_string(Y1," ",".",L), verificarLlamada(L).
+
+verificarLlamada(L):- miembro("si", L), ayudar().
+verificarLlamada(L):- miembro("no", L), writeln("Gracias por utilizar nuestro servicio").
+verificarLlamada(L):- writeln("No entendi la peticion, intenta de nuevo."), read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L), verificarLlamada(L).
+
+%metodo principal
+iniciar :- read(Y),string_lower(Y,Y1),split_string(Y1," ",".,",L),writeln(L), verificarAccion(L).
+
+test :- readln(Y,_,_,_,lowercase), writeln(Y),oracion(L, []).
+
 %[despegar, matricula, avion, hora]
 %[aterrizar, matricula, avion, hora]
-%[mayday, emergencia, matricula,avion]
+%[mayday, emergencia, matricula,avion]"
